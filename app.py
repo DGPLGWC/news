bca17e92d4a7211a88543b01805e9c84597c8fae
rsreddy <radhika.b.shah@gmail.com>
2017-05-31 01:39:21 -0500

    updated colors and links

--- app.py
+++ app.py
@@ -12,35 +12,35 @@ def index():
 def environment():
     return render_template(
         'environment.html',
-        topic = 'environment'
+        topic = 'Environment'
     )
 
 @app.route('/equality')
 def equality():
     return render_template(
         'equality.html',
-        topic = 'equality'
+        topic = 'Equality'
     )
 
 @app.route('/education')
 def education():
     return render_template(
         'education.html',
-        topic = 'education'
+        topic = 'Education'
     )
 
 @app.route('/feminism')
 def feminism():
     return render_template(
         'feminism.html',
-        topic = 'feminism'
+        topic = 'Feminism'
     )
 
 @app.route('/science')
 def science():
     return render_template(
         'science.html',
-        topic = 'science'
+        topic = 'Science'
     )
 app.run(host = os.getenv('IP','0.0.0.0'), port = int(os.getenv('PORT',8080)))
 
